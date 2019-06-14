0996579a323e0e518f2102b8dca93b81fbb87788
Luan Alves <luan.alves@comunike.me>
2019-06-14 11:18:34 -0300

    feat: Router

--- API_Spoiler/index.js
+++ API_Spoiler/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const express = require("express");
+const spoilersRoute = require("../API_Spoiler/src/routes/spoiler");
 
 const app = express();
 const hostname = "127.0.0.1";
@@ -7,8 +8,16 @@ const port = 3000;
 
 app.set("port", port);
 
+app.use(express.json());
+
+app.use("/api", spoilersRoute);
+
 app.use((request, response, next) => {
   response.status(404).send();
+});
+
+app.use((error, request, response, next) => {
+  response.status(500).json({ error });
 });
 
 const server = http.createServer(app);
